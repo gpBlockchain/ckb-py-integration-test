5184ba2af7b256200a74a0c6b2e1cb45a7233b9c
gpBlockchain <32102187+gpBlockchain@users.noreply.github.com>
2024-03-20 09:52:24 +0800

    add 115 node  (#34)
    * add  115 node

--- download.py
+++ download.py
@@ -11,7 +11,7 @@ import zipfile
 import requests
 from tqdm import tqdm
 
-versions = ['0.109.0', '0.110.2', '0.111.0', '0.112.1', '0.113.1', "0.114.0"]  # Replace with your versions
+versions = ['0.109.0', '0.110.2', '0.111.0', '0.112.1', '0.113.1', "0.114.0", "ckb 0.115.0-rc1"]  # Replace with your versions
 
 DOWNLOAD_DIR = "download"
 SYSTEMS = {
